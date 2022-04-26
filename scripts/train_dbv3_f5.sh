692eb1b52f9d734fb5b88a954bc799102cae7299
Jeamee <59476866+Jeamee@users.noreply.github.com>
2022-04-26 20:34:45 +0800

    modied script

--- scripts/train_dbv3_f5.sh
+++ scripts/train_dbv3_f5.sh
@@ -1,15 +1,16 @@
 set -Eeuox
-DATE=0413
+DATE=0426
 DECODER=softmax
 BS=8
 MAX_LEN=512
 MODEL="microsoft/deberta-v3-large"
-DESC="norm"
 
-for FOLD in 0 1;do
+
+for FOLD in 0 1 2 3 4;do
+DESC=norm-$FOLD
 python ../src/train.py --fold $FOLD --model $MODEL --decoder $DECODER --warmup_ratio 0.05 \
  --trans_lr 1e-5 --epochs 10 --max_len $MAX_LEN --batch_size $BS --valid_batch_size 32 \
  --desc $DESC \
 --input_csv /workspace/train_folds.csv \
---output /workspace/${MODEL#*/}_${DECODER}_bs${BS}_ml${MAX_LEN}_oof${FOLD}_${DATE}_${DESC}
+--output /workspace/${MODEL#*/}_${DECODER}_bs${BS}_ml${MAX_LEN}_${DATE}_${DESC}
 done
