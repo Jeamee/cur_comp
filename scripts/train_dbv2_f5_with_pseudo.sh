a4d155bbc3e7140150c07d62b9e70083fe490bbe
jeamee <jeamee@qq.com>
2022-05-02 01:16:57 +0800

    change warmup steps

--- scripts/train_dbv2_f5_with_pseudo.sh
+++ scripts/train_dbv2_f5_with_pseudo.sh
@@ -8,7 +8,7 @@ MODEL="microsoft/deberta-v2-xlarge"
 
 for FOLD in 0 1 2 3 4;do
     DESC=pseudo-$FOLD
-    python ../src/train.py --fold $FOLD --model $MODEL --decoder $DECODER --warmup_ratio 300 \
+    python ../src/train.py --fold $FOLD --model $MODEL --decoder $DECODER --warmup_ratio 900 \
     --trans_lr 5e-6 --epochs 1 --max_len $MAX_LEN --batch_size $BS --valid_batch_size 32 \
     --clip_grad_norm 1.0 --gradient_ckpt --pseudo_csv /workspace/pseudo_oof${FOLD}.pkl --val_check_interval 300 \
     --desc $DESC --lr_decay 0.95 \
