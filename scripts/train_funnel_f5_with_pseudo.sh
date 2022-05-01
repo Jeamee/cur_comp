d90a4d21cb32e43f7d7c3ed8b570e938979dc437
jeamee <jeamee@qq.com>
2022-05-01 23:28:15 +0800

    remove funnel gradient ckpt

--- scripts/train_funnel_f5_with_pseudo.sh
+++ scripts/train_funnel_f5_with_pseudo.sh
@@ -9,7 +9,7 @@ for FOLD in 0 1 2 3 4;do
 DESC=pseudo-$FOLD
     python ../src/train.py --fold $FOLD --model $MODEL --decoder $DECODER --warmup_ratio 600 \
     --trans_lr 1e-5 --epochs 1 --max_len $MAX_LEN --batch_size $BS --valid_batch_size 32 --clip_grad_norm 1.0 \
-    --gradient_ckpt --pseudo_csv /workspace/pseudo_oof${FOLD}.pkl --val_check_interval 300 \
+    --pseudo_csv /workspace/pseudo_oof${FOLD}.pkl --val_check_interval 300 \
     --desc $DESC --lr_decay 0.95 \
     --input_csv /workspace/train_folds.csv \
     --output /workspace/${MODEL#*/}_pseudo_${DECODER}_bs${BS}_ml${MAX_LEN}_${DATE}_${DESC}
