db6e3430edf57b14345fe97b94e514a49b09902b
jeamee <jeamee@qq.com>
2022-05-01 20:47:58 +0800

    change script

--- scripts/train_dbv3_f5_with_pseudo.sh
+++ scripts/train_dbv3_f5_with_pseudo.sh
@@ -1,13 +1,13 @@
 set -Eeuox
 DATE=0501
 DECODER=softmax
-BS=32
+BS=16
 MAX_LEN=512
-MODEL="google/electra-large-discriminator"
+MODEL="microsoft/deberta-v3-large"
 FOLD=0
 
 DESC=pseudo-$FOLD
-python ../src/train.py --fold $FOLD --model $MODEL --decoder $DECODER --warmup_ratio 500 \
+python ../src/train.py --fold $FOLD --model $MODEL --decoder $DECODER --warmup_ratio 600 \
  --trans_lr 1e-5 --epochs 10 --max_len $MAX_LEN --batch_size $BS --valid_batch_size 32 --clip_grad_norm 1.0 \
  --gradient_ckpt --pseudo_csv /workspace/pseudo_oof${FOLD}.pkl --val_check_interval 300 \
  --desc $DESC \
