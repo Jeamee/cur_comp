29b3d16c9059d556f8a1266699e60cb99f0896c9
jeamee <jeamee@qq.com>
2022-05-02 01:15:46 +0800

    bug fix

--- src/utils.py
+++ src/utils.py
@@ -501,5 +501,7 @@ def get_cosine_schedule_with_warmup(
         if current_step >= num_warmup_steps:
             progress = float(current_step - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
             return max(0.0, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress)))
+        else:
+            return 1.0
 
     return LambdaLR(optimizer, lr_lambda, last_epoch)
